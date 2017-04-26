ce8711e93c5eeef4b6281c6158d1e1223f43c3ef
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-04-26 17:14:31 -0400

    delete key and backspace functional

--- kilo.c
+++ kilo.c
@@ -22,6 +22,7 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 #define KILO_VERSION "0.0.1"
 #define KILO_TAB_STOP 8
+#define KILO_QUIT_TIMES 3
 
 enum editorKey
 {
@@ -57,6 +58,7 @@ struct editorConfig
   int screencols;
   int numrows;
   erow *row;
+  int dirty;
   char *filename;
   char statusmsg[80];
   time_t statusmsg_time;
@@ -311,6 +313,25 @@ void editorAppendRow(char *s, size_t len)
   editorUpdateRow(&E.row[at]);
 
   E.numrows++;
+  E.dirty++;
+}
+
+void editorFreeRow(erow *row)
+{
+  free(row->render);
+  free(row->chars);
+}
+
+void editorDelRow(int at)
+{
+  if (at < 0 || at >= E.numrows)
+  {
+    return;
+  }
+  editorFreeRow(&E.row[at]);
+  memmove(&E.row[at], &E.row[at + 1], sizeof(erow) * (E.numrows - at - 1));
+  E.numrows--;
+  E.dirty++;
 }
 
 void editorRowInsertChar(erow *row, int at, int c)
@@ -324,6 +345,30 @@ void editorRowInsertChar(erow *row, int at, int c)
   row->size++;
   row->chars[at] = c;
   editorUpdateRow(row);
+  E.dirty++;
+}
+
+void editorRowAppendString(erow *row, char *s, size_t len)
+{
+  row->chars = realloc(row->chars, row->size + len + 1);
+  memcpy(&row->chars[row->size], s, len);
+  row->size += len;
+  row->chars[row->size] = '\0';
+  editorUpdateRow(row);
+  E.dirty++;
+}
+
+void editorRowDelChar(erow *row, int at)
+{
+  if (at < 0 || at >= row->size)
+  {
+    return;
+  }
+
+  memmove(&row->chars[at], &row->chars[at + 1], row->size - at);
+  row->size--;
+  editorUpdateRow(row);
+  E.dirty++;
 }
 
 /* ** editor operations ***/
@@ -336,6 +381,32 @@ void editorInsertChar(int c)
   }
   editorRowInsertChar(&E.row[E.cy], E.cx, c);
   E.cx++;
+}
+
+void editorDelChar()
+{
+  if (E.cy == E.numrows)
+  {
+    return;
+  }
+  if (E.cx == 0 && E.cy == 0)
+  {
+    return;
+  }
+
+  erow *row = &E.row[E.cy];
+  if (E.cx > 0)
+  {
+    editorRowDelChar(row, E.cx - 1);
+    E.cx--;
+  }
+  else
+  {
+    E.cx = E.row[E.cy - 1].size;
+    editorRowAppendString(&E.row[E.cy - 1], row->chars, row->size);
+    editorDelRow(E.cy);
+    E.cy--;
+  }
 }
 
 /* ** file i/o ***/
@@ -391,6 +462,7 @@ void editorOpen(char *filename)
   }
   free(line);
   fclose(fp);
+  E.dirty = 0;
 }
 
 void editorSave()
@@ -412,6 +484,7 @@ void editorSave()
       {
         close(fd);
         free(buf);
+        E.dirty = 0;
         editorSetStatusMessage("%d bytes written to disk", len);
         return;
       }
@@ -538,7 +611,9 @@ void editorDrawStatusBar(struct abuf *ab)
 {
   abAppend(ab, "\x1b[7m", 4);
   char status[80], rstatus[80];
-  int len = snprintf(status, sizeof(status), "%.20s - %d lines", E.filename ? E.filename : "[No Name]", E.numrows);
+  int len = snprintf(status, sizeof(status), "%.20s - %d lines",
+      E.filename ? E.filename : "[No Name]", E.numrows,
+      E.dirty ? "(modified)" : "");
   int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d", E.cy + 1, E.numrows);
   if (len > E.screencols)
   {
@@ -661,6 +736,8 @@ void editorMoveCursor(int key)
 
 void editorProcessKeypress()
 {
+  static int quit_times = KILO_QUIT_TIMES;
+
   int c = editorReadKey();
 
   switch (c)
@@ -670,6 +747,13 @@ void editorProcessKeypress()
       break;
 
     case CTRL_KEY('q'):
+      if (E.dirty && quit_times > 0)
+      {
+        editorSetStatusMessage("WARNING!!! File has unsaved changes. "
+            "Press Ctrl-Q %d more times to quit.", quit_times);
+        quit_times--;
+        return;
+      }
       write(STDOUT_FILENO, "\x1b[2J", 4);
       write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
@@ -693,7 +777,11 @@ void editorProcessKeypress()
     case BACKSPACE:
     case CTRL_KEY('h'):
     case DEL_KEY:
-      /* todo */
+      if (c == DEL_KEY)
+      {
+        editorMoveCursor(ARROW_RIGHT);
+      }
+      editorDelChar();
       break;
 
     case PAGE_UP:
@@ -735,6 +823,8 @@ void editorProcessKeypress()
       editorInsertChar(c);
       break;
   }
+
+  quit_times = KILO_QUIT_TIMES;
 }
 
 /* ** init ***/
@@ -748,6 +838,7 @@ void initEditor()
   E.coloff = 0;
   E.numrows = 0;
   E.row = NULL;
+  E.dirty = 0;
   E.filename = NULL;
   E.statusmsg[0] = '\0';
   E.statusmsg_time = 0;
