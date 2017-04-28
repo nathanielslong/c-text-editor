3fc488a8701c2db292a74067ec273d3631398744
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-04-28 10:59:45 -0400

    basic text editing and saving added

--- kilo.c
+++ kilo.c
@@ -70,6 +70,8 @@ struct editorConfig E;
 /* ** prototypes ***/
 
 void editorSetStatusMessage(const char *fmt, ...);
+void editorRefreshScreen();
+char *editorPrompt(char *prompt);
 
 /* ** terminal ***/
 
@@ -298,11 +300,16 @@ void editorUpdateRow(erow *row)
   row->rsize = idx;
 }
 
-void editorAppendRow(char *s, size_t len)
+void editorInsertRow(int at, char *s, size_t len)
 {
-  E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+  if (at < 0 || at > E.numrows)
+  {
+    return;
+  }
 
-  int at = E.numrows;
+  E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+  memmove(&E.row[at + 1], &E.row[at], sizeof(erow) * (E.numrows - at));
+
   E.row[at].size = len;
   E.row[at].chars = malloc(len + 1);
   memcpy(E.row[at].chars, s, len);
@@ -377,10 +384,29 @@ void editorInsertChar(int c)
 {
   if (E.cy == E.numrows)
   {
-    editorAppendRow("", 0);
+    editorInsertRow(E.numrows, "", 0);
   }
   editorRowInsertChar(&E.row[E.cy], E.cx, c);
   E.cx++;
+}
+
+void editorInsertNewLine()
+{
+  if (E.cx == 0)
+  {
+    editorInsertRow(E.cy, "", 0);
+  }
+  else
+  {
+    erow *row = &E.row[E.cy];
+    editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx);
+    row = &E.row[E.cy];
+    row->size = E.cx;
+    row->chars[row->size] = '\0';
+    editorUpdateRow(row);
+  }
+  E.cy++;
+  E.cx = 0;
 }
 
 void editorDelChar()
@@ -456,8 +482,8 @@ void editorOpen(char *filename)
       while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r'))
       {
         linelen--;
-        editorAppendRow(line, linelen);
       }
+      editorInsertRow(E.numrows, line, linelen);
     }
   }
   free(line);
@@ -469,7 +495,12 @@ void editorSave()
 {
   if (E.filename == NULL)
   {
-    return;
+    E.filename = editorPrompt("Save as: %s (ESC to cancel)");
+    if (E.filename == NULL)
+    {
+      editorSetStatusMessage("Save aborted");
+      return;
+    }
   }
 
   int len;
@@ -685,6 +716,53 @@ void editorSetStatusMessage(const char *fmt, ...)
 
 /* ** input ***/
 
+char *editorPrompt(char *prompt)
+{
+  size_t bufsize = 128;
+  char *buf = malloc(bufsize);
+
+  size_t buflen = 0;
+  buf[0] = '\0';
+
+  while (1)
+  {
+    editorSetStatusMessage(prompt, buf);
+    editorRefreshScreen();
+
+    int c = editorReadKey();
+    if (c == DEL_KEY || c == CTRL_KEY('h') || c == BACKSPACE)
+    {
+      if (buflen != 0)
+      {
+        buf[--buflen] = '\0';
+      }
+    }
+    else if (c == '\x1b') {
+        editorSetStatusMessage("");
+        free(buf);
+        return NULL;
+    }
+    else if (c == '\r')
+    {
+      if (buflen != 0)
+      {
+        editorSetStatusMessage("");
+        return buf;
+      }
+    }
+    else if (!iscntrl(c) && c < 128)
+    {
+      if (buflen == bufsize - 1)
+      {
+        bufsize *= 2;
+        buf = realloc(buf, bufsize);
+      }
+      buf[buflen++] = c;
+      buf[buflen] = '\0';
+    }
+  }
+}
+
 void editorMoveCursor(int key)
 {
   erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
@@ -743,7 +821,7 @@ void editorProcessKeypress()
   switch (c)
   {
     case '\r':
-      /* todo */
+      editorInsertNewline();
       break;
 
     case CTRL_KEY('q'):
