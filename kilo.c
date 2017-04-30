e870c420ffb45c2850659e9586f441985e7a3d08
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-04-30 18:08:46 -0400

    comments now highlighted

--- kilo.c
+++ kilo.c
@@ -42,11 +42,14 @@ enum editorKey
 enum editorHighlight
 {
   HL_NORMAL = 0,
+  HL_COMMENT,
+  HL_STRING,
   HL_NUMBER,
   HL_MATCH
 };
 
 #define HL_HIGHLIGHT_NUMBERS (1<<0)
+#define HL_HIGHLIGHT_STRINGS (1<<1)
 
 /*** data ***/
 
@@ -54,6 +57,7 @@ struct editorSyntax
 {
   char *filetype;
   char **filematch;
+  char *singleline_comment_start;
   int flags;
 };
 
@@ -94,7 +98,8 @@ struct editorSyntax HLDB[] = {
   {
     "c",
     C_HL_extensions,
-    HL_HIGHLIGHT_NUMBERS
+    "//",
+    HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
   },
 };
 
@@ -297,13 +302,57 @@ void editorUpdateSyntax(erow *row)
     return;
   }
 
+  char *scs = E.syntax->singleline_comment_start;
+  int scs_len = scs ? strlen(scs) : 0;
+
   int prev_sep = 1;
+  int in_string = 0;
 
   int i = 0;
   while (i < row->rsize)
   {
     char c = row->render[i];
     unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
+
+    if (scs_len && !in_string)
+    {
+      if (!strncmp(&row->render[i], scs, scs_len))
+      {
+        memset(&row->hl[i], HL_COMMENT, row->rsize - i);
+        break;
+      }
+    }
+
+    if (E.syntax->flags & HL_HIGHLIGHT_STRINGS)
+    {
+      if (in_string)
+      {
+        row->hl[i] = HL_STRING;
+        if (c == '\\' && i + 1 < row->size)
+        {
+          row->hl[i + 1] = HL_STRING;
+          i += 2;
+          continue;
+        }
+        if (c == in_string)
+        {
+          in_string = 0;
+        }
+        i++;
+        prev_sep = 1;
+        continue;
+      }
+      else
+      {
+        if (c == '"' || c == '\'')
+        {
+          in_string = c;
+          row->hl[i] = HL_STRING;
+          i++;
+          continue;
+        }
+      }
+    }
 
     if (E.syntax->flags & HL_HIGHLIGHT_NUMBERS)
     {
@@ -326,6 +375,12 @@ int editorSyntaxToColor(int hl)
 {
   switch (hl)
   {
+    case HL_COMMENT:
+      return 36;
+
+    case HL_STRING:
+      return 35;
+
     case HL_NUMBER:
       return 31;
 
